7bfa71519cce5f17af01df1e5f9347c06fc84781
satosato-1 <aota.satoshi.32j@st.kyoto-u.ac.jp>
2025-12-30 03:25:38 +0900

    Fix setup.bat to ensure proper script execution

--- portable-python-vscode/setup.bat
+++ portable-python-vscode/setup.bat
@@ -1,4 +1,5 @@
 powershell.exe -ExecutionPolicy Bypass -Command "Unblock-File -Path '%~dp0setup.ps1'"
 powershell.exe -ExecutionPolicy Bypass -File "%~dp0setup.ps1"
 echo --- Script finished ---
+
 pause
